f4c97b991e29baba554e649b5aa6f6fe9940e8b4
Animo105 <emeric2005leclerc@gmail.com>
2025-08-11 18:38:36 -0400

    save des items

--- Autoloads/expression_function.gd
+++ Autoloads/expression_function.gd
@@ -6,32 +6,25 @@ func increase_money(amount : int):
 
 func increase_coin_value(amount : int):
 	SlotsData.coins_bonus_value += amount
-	print("incrase coin value")
 
 func increase_fruits_mult(amount : float):
 	SlotsData.fruits_mult_bonus += amount
-	print("incrase fruit mult value")
 
 func increase_luck(amount : float):
 	Global.luck_probability += amount
-	print("increase luck")
 
 func allow_bet(allow : bool):
 	Global.is_bet_allowed = allow
-	print("bet has been changed")
 
 func increase_slot_speed(amount : float):
 	Global.slot_wait_time -= amount
-	print("increase slot speed")
 
 func increase_capsule(type : int, amount : int)->int:
 	Global.capsules[type] += amount
-	print("Capsule earned")
 	return 0
 
 func decrease_base_gacha_price(amount : int):
 	Global.base_capsule_price -= amount
-	print("Derease base gacha game price")
 
 func increase_weight_for_slot(slot_id : int, weight_increase : float):
 	SlotsData.slots[slot_id].weight += weight_increase
--- Autoloads/save_manager.gd
+++ Autoloads/save_manager.gd
@@ -15,6 +15,7 @@ func save_data():
 	save_dic["capsules"] = Global.capsules
 	save_dic["level"] = Global.level
 	save_dic["exp"] = Global.experience
+	save_dic["items"] = Global.items_won
 	
 	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
 	if file:
@@ -35,22 +36,32 @@ func load_data():
 			var save_dic : Dictionary= JSON.parse_string(data)
 			print(save_dic)
 			# load
-		
+			
+			# load money
 			if save_dic.has("money"): Global.money = save_dic["money"]
+			# load le bet amount
 			if save_dic.has("bet amount"): Global.bet_ammount = save_dic["bet amount"]
+			# load les upgrades acheté
 			if save_dic.has("upgrades"):
 				if save_dic["upgrades"] is Dictionary:
 					for upgrade in save_dic["upgrades"].keys():
 						Global.upgrades_bought[int(upgrade)] = int(save_dic["upgrades"][upgrade])
 					UpgradesData.apply_upgrades_bought()
+			# load les objects gagné
+			if save_dic.has("items"):
+				for key in save_dic["items"].keys():
+					Global.items_won[int(key)] = int(save_dic["items"][key])
+			# load le nombre de capsules
 			if save_dic.has("capsules"):
 				if save_dic["capsules"] is Dictionary:
 					for key in save_dic["capsules"]:
 						Global.capsules[int(key)] = int(save_dic["capsules"][key])
+			# laod les niveaux et l'exp
 			if save_dic.has("level"):
 				for i in range(save_dic["level"]):
 					Global.level_up()
 			if save_dic.has("exp"): Global.experience = save_dic["exp"]
+			
 			
 func reset_data():
 	if DirAccess.remove_absolute(SAVE_PATH) == OK:
